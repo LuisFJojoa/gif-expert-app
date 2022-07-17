73c7c9c4f2338f658f581a93b1f793abc11ebba6
Fernando Jojoa <luis.jojoa.q@gmail.com>
2022-07-17 14:31:46 -0500

    delete api_key giphy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=u7Yrmgn9RUtwW7p01x77dAjVvIUHB1md&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=API_KEY&q=${category}&limit=10`;
     const request = await fetch( url )
     const { data } = await request.json()
     const gifs = data.map(img => ({
